d277067b7910935445cde5a8b88db17e554e892d
Abdulkareem Abdulkareem <abdulkareemabdulkareem778@gmail.com>
2025-10-06 13:10:44 +0300

    gjhhjhjhcv

--- script.js
+++ script.js
@@ -3,5 +3,3 @@ const menuToggle = document.getElementById('menu-toggle');
 const nav = document.getElementById('nav');
 menuToggle?.addEventListener('click', ()=> nav.classList.toggle('show'));
 
-// fill year
-document.getElementById('year').textContent = new Date().getFullYear();
